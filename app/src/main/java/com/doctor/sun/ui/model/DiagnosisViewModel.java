bd05dce81087146f189da6ff415d35b207d6fb1b
rick <rick@ganguo.hk>
2016-04-19 15:41:06 +0800

    医生建议不能保存问题 (cherry picked from commit eff7f25)

--- app/src/main/java/com/doctor/sun/ui/model/DiagnosisViewModel.java
+++ app/src/main/java/com/doctor/sun/ui/model/DiagnosisViewModel.java
@@ -325,7 +325,7 @@ public class DiagnosisViewModel {
     public HashMap<String, String> toHashMap(Appointment appointmentId, FragmentDiagnosisBinding binding, String prescriptions) {
         HashMap<String, String> result = new HashMap<>();
         result.put("appointmentId", String.valueOf(appointmentId.getId()));
-//        result.put("is_diagnosis", binding.isDiagnosis.getIsChecked() ? "1" : "0");
+        result.put("is_diagnosis", "1");
         result.put("perception", perception.toStates());
         result.put("thinking", thinking.toStates());
         result.put("pipedream", pipedream.toStates());
