ccf4baf3b894c5c23c885dcf45ae58b78aea20ca
rick <rick@ganguo.hk>
2016-07-19 12:21:29 +0800

    1.修改在oppo上面显示vectorDrawable的bug

--- app/src/main/java/com/doctor/sun/ui/binding/CustomBinding.java
+++ app/src/main/java/com/doctor/sun/ui/binding/CustomBinding.java
@@ -1,8 +1,11 @@
 package com.doctor.sun.ui.binding;
 
+import android.annotation.TargetApi;
+import android.content.res.Resources;
 import android.databinding.BindingAdapter;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.DrawableRes;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
@@ -53,6 +56,68 @@ public class CustomBinding {
         Drawable d = VectorDrawableCompat.create(imageView.getResources(), resourceId, null);
         Drawable dWrap = DrawableCompat.wrap(d);
         imageView.setImageDrawable(dWrap);
+    }
+
+
+    @BindingAdapter(value = "app:backgroundCompat")
+    public static void backgroundCompat(View view, @DrawableRes int background) {
+        Drawable d = VectorDrawableCompat.create(view.getResources(), background, null);
+        Drawable dWrap = DrawableCompat.wrap(d);
+        if (dWrap != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                view.setBackground(dWrap);
+            } else {
+                view.setBackgroundDrawable(dWrap);
+            }
+        }
+    }
+
+    @BindingAdapter(value = "app:drawableTop")
+    public static void drawableTop(TextView textView, @DrawableRes int top) {
+        setCompoundVectorDrawables(textView, 0, top, 0, 0);
+    }
+
+    /**
+     * AppCompat 23.3.0 removed support of vector drawables attributes like
+     * drawableLeft, from xml.  Use this method to use a vector drawable as
+     * a compound drawable of a TextView.
+     */
+    public static void setCompoundVectorDrawables(TextView textView,
+                                                  @DrawableRes int start,
+                                                  @DrawableRes int top,
+                                                  @DrawableRes int end,
+                                                  @DrawableRes int bottom) {
+        // On Marshmallow, if we use the setCompoundDrawables* methods which take
+        // VectorDrawableCompat arguments, the rendering is incorrect (the text
+        // appears on top of the icon).  If we use the setCompoundDrawable* methods
+        // which take resource ids instead, we don't have this problem. Since the
+        // method which takes resource ids is available on lollipop, we use
+        // it starting from lollipop.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setCompoundVectorDrawablesV21(textView, start, top, end, bottom);
+        } else {
+            Resources resources = textView.getResources();
+            android.support.v4.widget.TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    textView,
+                    createVectorDrawable(resources, start),
+                    createVectorDrawable(resources, top),
+                    createVectorDrawable(resources, end),
+                    createVectorDrawable(resources, bottom));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static void setCompoundVectorDrawablesV21(TextView textView,
+                                                      @DrawableRes int start,
+                                                      @DrawableRes int top,
+                                                      @DrawableRes int end,
+                                                      @DrawableRes int bottom) {
+        textView.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+    }
+
+    private static VectorDrawableCompat createVectorDrawable(Resources resources, @DrawableRes int res) {
+        if (res == 0) return null;
+        return VectorDrawableCompat.create(resources, res, null);
     }
 
     @android.databinding.BindingAdapter("bind:onClick")
