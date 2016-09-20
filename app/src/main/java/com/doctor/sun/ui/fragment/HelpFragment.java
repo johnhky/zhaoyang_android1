1928dca3323ccba64d359b236af3240d3f76a839
rick <rick@ganguo.hk>
2016-09-20 16:06:34 +0800

    删除旧的帮助教程

--- app/src/main/java/com/doctor/sun/ui/fragment/HelpFragment.java
+++ app/src/main/java/com/doctor/sun/ui/fragment/HelpFragment.java
@@ -1,41 +1,41 @@
-package com.doctor.sun.ui.fragment;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.bumptech.glide.Glide;
-import com.doctor.sun.R;
-import com.doctor.sun.databinding.FragmentHelpBinding;
-
-/**
- * Created by lucas on 2/3/16.
- */
-public class HelpFragment extends BaseFragment {
-    public static final String IMAGE_ID = "IMAGE_ID";
-    private FragmentHelpBinding binding;
-
-    public static HelpFragment newInstance(String id) {
-
-        Bundle args = new Bundle();
-        args.putString(IMAGE_ID, id);
-        HelpFragment fragment = new HelpFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public String getDrawableUrl() {
-        String imageId = getArguments().getString(IMAGE_ID);
-        return imageId;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentHelpBinding.inflate(inflater, container, false);
-        Glide.with(this).load(getDrawableUrl()).placeholder(R.drawable.bg_default).into(binding.ivHelp);
-        return binding.getRoot();
-    }
-}
+//package com.doctor.sun.ui.fragment;
+//
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.bumptech.glide.Glide;
+//import com.doctor.sun.R;
+//import com.doctor.sun.databinding.FragmentHelpBinding;
+//
+///**
+// * Created by lucas on 2/3/16.
+// */
+//public class HelpFragment extends BaseFragment {
+//    public static final String IMAGE_ID = "IMAGE_ID";
+//    private FragmentHelpBinding binding;
+//
+//    public static HelpFragment newInstance(String id) {
+//
+//        Bundle args = new Bundle();
+//        args.putString(IMAGE_ID, id);
+//        HelpFragment fragment = new HelpFragment();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    public String getDrawableUrl() {
+//        String imageId = getArguments().getString(IMAGE_ID);
+//        return imageId;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        binding = FragmentHelpBinding.inflate(inflater, container, false);
+//        Glide.with(this).load(getDrawableUrl()).placeholder(R.drawable.bg_default).into(binding.ivHelp);
+//        return binding.getRoot();
+//    }
+//}
