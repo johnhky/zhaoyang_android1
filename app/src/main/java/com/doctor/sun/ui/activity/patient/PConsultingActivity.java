e2329da851d8f2f3ef4ad2b1b649a789de6b2a6b
rick <contou.y@gmail.com>
2016-10-08 17:25:10 +0800

    1.患者端就诊进行中，去掉通讯录入口

--- app/src/main/java/com/doctor/sun/ui/activity/patient/PConsultingActivity.java
+++ app/src/main/java/com/doctor/sun/ui/activity/patient/PConsultingActivity.java
@@ -6,15 +6,12 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.doctor.sun.R;
 import com.doctor.sun.bean.Constants;
 import com.doctor.sun.databinding.ActivityConsultationBinding;
 import com.doctor.sun.event.ShowCaseFinishedEvent;
 import com.doctor.sun.ui.activity.BaseFragmentActivity2;
-import com.doctor.sun.ui.activity.doctor.ContactActivity;
 import com.doctor.sun.ui.fragment.ConsultingFragment2;
 import com.doctor.sun.ui.model.FooterViewModel;
 import com.doctor.sun.ui.model.PatientFooterView;
@@ -52,10 +49,6 @@ public class PConsultingActivity extends BaseFragmentActivity2 {
         return FooterViewModel.getInstance(new PatientFooterView(this), R.id.tab_two);
     }
 
-    public void onMenuClicked() {
-        Intent intent = ContactActivity.makeIntent(this, ContactActivity.PATIENTS_CONTACT);
-        startActivity(intent);
-    }
 
     @Override
     public void onResume() {
@@ -73,23 +66,27 @@ public class PConsultingActivity extends BaseFragmentActivity2 {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_consulting, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_contact: {
-                onMenuClicked();
-                return true;
-            }
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_consulting, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_contact: {
+//                onMenuClicked();
+//                return true;
+//            }
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//    public void onMenuClicked() {
+//        Intent intent = ContactActivity.makeIntent(this, ContactActivity.PATIENTS_CONTACT);
+//        startActivity(intent);
+//    }
 
     @Override
     public int getMidTitle() {
