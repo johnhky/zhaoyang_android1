109e14dcd4469f8ba9fc3fd072b4bc3ac7b1186e
fantasticKB <helloiamkb@gmail.com>
2016-10-08 16:32:29 +0800

    【公众端】随访住院时间默认为填写问卷当天

--- app/src/main/java/com/doctor/sun/vo/ItemPickDate.java
+++ app/src/main/java/com/doctor/sun/vo/ItemPickDate.java
@@ -100,6 +100,10 @@ public class ItemPickDate extends BaseItem {
         return String.format(Locale.CHINA, "%04d-%02d", year, monthOfYear + 1);
     }
 
+    public String getToday() {
+        return String.format(Locale.CHINA, "%04d-%02d-%02d", year + 18, monthOfYear + 1, dayOfMonth);
+    }
+
     public String getBirthday() {
         return getDate();
     }
