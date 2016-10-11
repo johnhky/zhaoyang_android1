33de1c22eab56ae004dc0b3920a1b9fdfc98eae5
rick <contou.y@gmail.com>
2016-10-11 16:45:55 +0800

    1.增加leakcannery

--- app/src/main/java/com/doctor/sun/AppContext.java
+++ app/src/main/java/com/doctor/sun/AppContext.java
@@ -17,6 +17,7 @@ import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.SDKOptions;
 import com.netease.nimlib.sdk.msg.MsgService;
 import com.netease.nimlib.sdk.msg.MsgServiceObserve;
+import com.squareup.leakcanary.LeakCanary;
 import com.squareup.otto.Subscribe;
 import com.umeng.analytics.MobclickAgent;
 
@@ -44,6 +45,12 @@ public class AppContext extends BaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         AppEnv.init(this);
         // init libs
