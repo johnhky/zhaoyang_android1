ad725e55c6f52b0660f02d376687d69eb07ea62d
fantasticKB <helloiamkb@gmail.com>
2016-10-27 16:26:28 +0800

    =layout：【医生端】查看量表界面添加查看结果的fab

--- app/src/main/java/com/doctor/sun/ui/activity/LeftDrawerFragmentActivity.java
+++ app/src/main/java/com/doctor/sun/ui/activity/LeftDrawerFragmentActivity.java
@@ -5,9 +5,14 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Toast;
 
 import com.doctor.sun.R;
 import com.doctor.sun.bean.Constants;
+import com.doctor.sun.databinding.ActivityLeftDrawerWraperBinding;
 import com.doctor.sun.util.FragmentFactory;
 
 /**
@@ -15,6 +20,7 @@ import com.doctor.sun.util.FragmentFactory;
  */
 
 public class LeftDrawerFragmentActivity extends BaseFragmentActivity2 {
+    private ActivityLeftDrawerWraperBinding binding;
 
     public static Intent intentFor(Context context, String title, Bundle contentArgs, Bundle drawerArgs) {
         Intent intent = new Intent(context, LeftDrawerFragmentActivity.class);
@@ -30,7 +36,7 @@ public class LeftDrawerFragmentActivity extends BaseFragmentActivity2 {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.activity_left_drawer_wraper);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_left_drawer_wraper);
 
 
         Bundle contentArgs = getIntent().getBundleExtra(Constants.FRAGMENT_CONTENT_BUNDLE);
@@ -38,6 +44,20 @@ public class LeftDrawerFragmentActivity extends BaseFragmentActivity2 {
 
         Bundle leftDrawerArgs = getIntent().getBundleExtra(Constants.FRAGMENT_LEFT_DRAWER_BUNDLE);
         leftDrawer = FragmentFactory.getInstance().get(leftDrawerArgs);
+
+        binding.fab.setOnInflateListener(new ViewStub.OnInflateListener() {
+            @Override
+            public void onInflate(ViewStub stub, View inflated) {
+                Log.e(TAG, "OnInflate: ");
+            }
+        });
+        View view = binding.fab.getViewStub().inflate();
+        view.findViewById(R.id.fab_view_result).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "FAB", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         getSupportFragmentManager()
                 .beginTransaction()
