817a9c5161a834f93eb787f8172a0372621d5412
rick <contou.y@gmail.com>
2016-11-08 11:27:47 +0800

    =activity:在登陆界面也检测一下更新

--- app/src/main/java/com/doctor/sun/ui/activity/LoginActivity.java
+++ app/src/main/java/com/doctor/sun/ui/activity/LoginActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 
 import com.doctor.sun.R;
 import com.doctor.sun.databinding.ActivityLoginBinding;
+import com.doctor.sun.event.UpdateEvent;
 import com.doctor.sun.ui.handler.LoginHandler;
+import com.doctor.sun.util.UpdateUtil;
+import com.squareup.otto.Subscribe;
 
 
 /**
@@ -30,5 +33,24 @@ public class LoginActivity extends BaseFragmentActivity2 {
         handler = new LoginHandler();
         binding.setHandler(handler);
 
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (shouldCheck()) {
+            UpdateUtil.checkUpdate(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UpdateUtil.onPause();
+    }
+
+    @Subscribe
+    public void onUpdateEvent(UpdateEvent e) {
+        UpdateUtil.handleNewVersion(this, e.getData(), e.getVersionName());
     }
 }
