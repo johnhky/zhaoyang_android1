e4ec93af8a78405ea1c98504c7f6d31f56c131c4
fantasticKB <helloiamkb@gmail.com>
2016-11-11 12:07:58 +0800

    =activity.patient:修改寄药订单支付成功后，支付成功界面按钮的文案

--- app/src/main/java/com/doctor/sun/ui/activity/patient/PAppointmentListActivity.java
+++ app/src/main/java/com/doctor/sun/ui/activity/patient/PAppointmentListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 
 import com.doctor.sun.R;
+import com.doctor.sun.bean.Constants;
 import com.doctor.sun.ui.activity.TabActivity;
 import com.doctor.sun.ui.pager.MyOrderPagerAdapter;
 
@@ -19,10 +20,17 @@ public class PAppointmentListActivity extends TabActivity {
         return i;
     }
 
+    public static Intent makeIntent(Context context, int position) {
+        Intent i = new Intent(context, PAppointmentListActivity.class);
+        i.putExtra(Constants.POSITION, position);
+        return i;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getBinding().vp.setOffscreenPageLimit(3);
+        getBinding().vp.setCurrentItem(getIntent().getIntExtra(Constants.POSITION, 0));
     }
 
     @Override
